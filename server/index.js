c91f29af60ce47387670638ade5adb71b37114e1
Ashik Ghosh <ashikghosh763@gmail.com>
2024-06-08 18:45:51 +0600

    done

--- server/index.js
+++ server/index.js
@@ -22,8 +22,20 @@ async function run() {
   try {
     const infos = client.db("dashboard").collection("infos");
 
-    app.get("/all-infos", async (req, res) => {
-      const result = await infos.findOne({ sector: "Energy" });
+    app.get("/all-sectors", async (req, res) => {
+      const result = await infos.aggregate(
+        [
+          {
+            $group: {
+              _id: "$sector", // Group documents by sector
+              total: {$sum:1}
+            }
+          }
+
+
+
+        ]
+      ).toArray();
       return res.send(result);
     });
 
@@ -39,6 +51,94 @@ async function run() {
           }
         }
       ]).toArray();
+      return res.send(result);
+    });
+
+
+    app.get("/insights-trends", async (req, res) => {
+      
+      const {startYear,endYear,sector}=req.query;
+      // console.log(req.query);
+      // const startYear = "2016";
+      // const endYear = "2016";
+      const result  = await infos.aggregate(
+        [
+          {
+              "$addFields": {
+                  "parsedDate": {
+                      "$dateFromString": {
+                          "dateString": "$added",
+                          "format": "%B, %d %Y %H:%M:%S"
+                      }
+                  }
+              }
+          },
+          {
+              "$match": {
+                  "sector": sector,
+                  "parsedDate": {
+                      "$gte": new Date(startYear + "-01-01T00:00:00Z"),
+                      "$lte": new Date(endYear + "-12-31T23:59:59Z")
+                  }
+              }
+          },
+          {
+              "$project": {
+                  "date": {
+                      "$dateToString": {
+                          "format": "%Y-%m-%d",
+                          "date": "$parsedDate"
+                      }
+                  },
+                  "topic": 1
+              }
+          },
+          {
+              "$group": {
+                  "_id": {
+                      "date": "$date",
+                      "topic": "$topic"
+                  },
+                  "count": { "$sum": 1 }
+              }
+          },
+          {
+              "$group": {
+                  "_id": "$_id.date",
+                  "topics": {
+                      "$push": {
+                          "topic": "$_id.topic",
+                          "count": "$count"
+                      }
+                  }
+              }
+          },
+          {
+              "$sort": {
+                  "_id": 1
+              }
+          },
+          {
+              "$project": {
+                  "_id": 0,
+                  "date": "$_id",
+                  "topics": {
+                      "$arrayToObject": {
+                          "$map": {
+                              "input": "$topics",
+                              "as": "t",
+                              "in": { "k": "$$t.topic", "v": "$$t.count" }
+                          }
+                      }
+                  }
+              }
+          }
+      ]
+  
+      ).toArray();
+
+    console.log(result);
+    
       return res.send(result);
     });
 
@@ -61,6 +161,9 @@ async function run() {
       console.log(result);
       return res.send(result);
     });
+
+
+
 
     console.log("clicked");
   } finally {
